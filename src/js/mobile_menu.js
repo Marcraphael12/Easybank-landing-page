875e193b07d0418ed0e5b24318631678942f8558
Marc raphael <marcraphael979@gmail.com>
2022-06-28 21:06:44 +0100

    js/mobile_menu: import navigation, import request invite

--- src/js/mobile_menu.js
+++ src/js/mobile_menu.js
@@ -1,8 +1,12 @@
 const burger_open = document.querySelector('.burger-open');
 const burger_close = document.querySelector('.burger-close');
 
+const navigation = document.querySelector('.navigation');
+const request_invite = document.querySelector('.request-invite');
+
 const Open_menu = (button) => {
 	button.addEventListener('click', () => {
-		
+		burger_open.classList.toggle('dn');
+		burger_close.classList.toggle('df-row');
 	})
 }
