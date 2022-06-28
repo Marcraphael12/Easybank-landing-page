bf8bf9fdd9d468f526baef657d6f242b936009f5
Marc raphael <marcraphael979@gmail.com>
2022-06-28 21:03:00 +0100

    js/mobile_menu: create the open event

---
+++ src/js/mobile_menu.js
@@ -0,0 +1,8 @@
+const burger_open = document.querySelector('.burger-open');
+const burger_close = document.querySelector('.burger-close');
+
+const Open_menu = (button) => {
+	button.addEventListener('click', () => {
+		
+	})
+}
