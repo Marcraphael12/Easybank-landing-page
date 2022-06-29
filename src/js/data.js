53204e88160a2b3df309b606f16712ca94a60b6a
Marc raphael <marcraphael979@gmail.com>
2022-06-29 12:08:12 +0100

    js/data: create datas for every cards

---
+++ src/js/data.js
@@ -0,0 +1,39 @@
+//images
+import confetti from '../public/icons/image-confetti.jpg';
+import currency from '../public/icons/image-currency.jpg';
+import plane from '../public/icons/image-plane.jpg';
+import restaurant from '../public/icons/image-restaurant.jpg';
+
+const Datas = [
+	{
+		id: 1,
+		image:`url(${currency})`,
+		author: 'By Claire Robinson',
+		title: 'Receive money in any currency with no fees',
+		text: 'The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single'
+	},
+
+	{
+		id: 2,
+		image:`url(${restaurant})`,
+		author: 'By Wilson Hutton',
+		title: 'Treat yourself without worrying about money',
+		text: 'Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you'
+	},
+
+	{
+		id: 3,
+		image:`url(${plane})`,
+		author: 'By Wilson Hutton',
+		title: 'Take your Easybank card wherever you go',
+		text: ' We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you'
+	},
+
+	{
+		id: 4,
+		image:`url(${confetti})`,
+		author: 'By Claire Robinson',
+		title: 'Our invite-only Beta accounts are now live!',
+		text: ' After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site'
+	},
+]
