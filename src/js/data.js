58621865afdd282cdfb31f8981c8192b49d1cafc
Marc raphael <marcraphael979@gmail.com>
2022-07-01 16:03:17 +0100

    js/data: use direct path to load images

--- src/js/data.js
+++ src/js/data.js
@@ -1,13 +1,7 @@
-//images
-import confetti from '../public/icons/image-confetti.jpg';
-import currency from '../public/icons/image-currency.jpg';
-import plane from '../public/icons/image-plane.jpg';
-import restaurant from '../public/icons/image-restaurant.jpg';
-
 const Datas = [
 	{
 		id: 1,
-		image:`url(${currency})`,
+		image:`./public/icons/image-currency.jpg`,
 		author: 'By Claire Robinson',
 		title: 'Receive money in any currency with no fees',
 		text: 'The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single'
@@ -15,7 +9,7 @@ const Datas = [
 
 	{
 		id: 2,
-		image:`url(${restaurant})`,
+		image:`./public/icons/image-restaurant.jpg`,
 		author: 'By Wilson Hutton',
 		title: 'Treat yourself without worrying about money',
 		text: 'Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you'
@@ -23,7 +17,7 @@ const Datas = [
 
 	{
 		id: 3,
-		image:`url(${plane})`,
+		image:`./public/icons/image-plane.jpg`,
 		author: 'By Wilson Hutton',
 		title: 'Take your Easybank card wherever you go',
 		text: ' We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you'
@@ -31,9 +25,11 @@ const Datas = [
 
 	{
 		id: 4,
-		image:`url(${confetti})`,
+		image:`./public/icons/image-confetti.jpg`,
 		author: 'By Claire Robinson',
 		title: 'Our invite-only Beta accounts are now live!',
 		text: ' After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site'
 	},
 ]
+
+export default Datas;
