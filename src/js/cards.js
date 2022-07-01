c651b360fca4e08dbd452fbefb8fd93f1ba431bf
Marc raphael <marcraphael979@gmail.com>
2022-07-01 16:04:48 +0100

    js/cards: use variables instead of xml syntax

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,4 +1,13 @@
 // articles datas
 import Datas from './data';
 
-const cards = document.querySelector('.cards');
+const cards = document.querySelector('.articles-container');
+
+export const card = Datas.map(article => {
+	const image = document.createElement('img');
+	image.setAttribute('src', `${article.image}`)
+	
+	const atcl = document.createElement('li');
+	atcl.appendChild(image);
+	cards.appendChild(atcl);
+});
